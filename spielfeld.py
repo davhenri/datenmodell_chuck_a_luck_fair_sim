479b03b1ff536dc806b112b2d04cb21e9b5c5fc1
David Henrich <150040600+davhenri@users.noreply.github.com>
2026-03-13 12:33:42 +0100

    Add Spielfeld class with methods for booking and counting hits

--- spielfeld.py
+++ spielfeld.py
@@ -1,1 +1,15 @@
+class Spielfeld(object):
+  def __init__(self):
+    self.feld =[1,2,3,4,5,6]
+    self.konto = 0
+    self.treffer = 0
 
+def buchen(self, wert):
+  self.konto += wert
+
+def trefferZaehlen(self, tipp, augen):
+  treffer = 0
+  for a in augen:
+    if tipp == a:
+      self.treffer += 1
+    
