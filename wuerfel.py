64387371a651c82d6f48b3aae8b6ee8860562b64
juschaub <schaub.julian@web.de>
2026-03-13 12:36:20 +0100

    Fix indentation and add docstrings in wuerfel.py

--- wuerfel.py
+++ wuerfel.py
@@ -4,12 +4,14 @@ class wuerfel(object):
 
    __slots__ = ('augen',)
 
+    """Initialisierung"""
     def __init__(self):
         self.augen = 0
  
+    """werfen() erzeugt eine W6-Zufallszahl(int)."""
     def werfen(self):
         self.augen = randint(1, 6)
-        return self.augen
 
+    """getAugen() gibt die Zufallszahl von werfen() aus."""
     def getAugen(self):
         return self.augen
