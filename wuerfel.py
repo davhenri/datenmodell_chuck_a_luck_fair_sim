a6d002a64ec3dda21156d82d377f749d9a50586b
juschaub <schaub.julian@web.de>
2026-03-13 12:32:08 +0100

    Add wuerfel class for dice rolling functionality

--- wuerfel.py
+++ wuerfel.py
@@ -1,1 +1,15 @@
+from random import randint
 
+class wuerfel(object):
+
+   __slots__ = ('augen',)
+
+    def __init__(self):
+        self.augen = 0
+ 
+    def werfen(self):
+        self.augen = randint(1, 6)
+        return self.augen
+
+    def getAugen(self):
+        return self.augen
